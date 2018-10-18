7f87312fba8a9d6b7078724bfab113e42e7a1617
Kenford <kzhou0066@gmail.com>
2018-10-18 05:45:01 -0400

    completed more solutions

--- Javascript Algorithms and Data Structures Certification/intermediate-algorithm-scripting/pig-latin.js
+++ Javascript Algorithms and Data Structures Certification/intermediate-algorithm-scripting/pig-latin.js
@@ -6,3 +6,24 @@
 
 // Input strings are guaranteed to be English words in all lowercase.
 
+function translatePigLatin(str) {
+    if(str[0] === "a" || str[0] === "e" || str[0] === "i" || str[0] === "o" || str[0] === "u")
+      return str + "way"
+    
+    if(!str.includes("a") && !str.includes("e") && !str.includes("i") && !str.includes("o") && !str.includes("u"))
+      return str + "ay";
+  
+     let consonantClusterLength = 1;
+     let clusterLetters = '';
+     for(let i = 1; i < str.length; i++){
+       if (str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" || str[i] === "u")
+         break;
+       else
+         consonantClusterLength++;
+     }
+    clusterLetters = str.slice(0, consonantClusterLength);
+    return str.slice(consonantClusterLength) + clusterLetters + "ay";
+  
+  }
+  
+  console.log(translatePigLatin("throw"));
