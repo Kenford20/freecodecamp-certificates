cde6f3639d345d07b14bd00ad1c64f51540f75bc
Kenford <kzhou0066@gmail.com>
2018-10-24 15:30:13 -0400

    added more solutions

--- Javascript Algorithms and Data Structures Certification/intermediate-algorithm-scripting/make-a-person.js
+++ Javascript Algorithms and Data Structures Certification/intermediate-algorithm-scripting/make-a-person.js
@@ -7,3 +7,30 @@
 
 // These methods must be the only available means of interacting with the object.
 
+var Person = function(name) {
+    // Complete the method below and implement the others similarly
+    let firstAndLast = name.split(" ");
+    
+    // getters
+    this.getFullName = function() {
+      return firstAndLast.join(' ');
+    };
+    this.getFirstName = () => {
+      return firstAndLast[0];
+    };
+    this.getLastName = () => {
+      return firstAndLast[1];
+    };
+    // setters
+    this.setFirstName = (firstname) => {
+      firstAndLast[0] = firstname;
+    };
+    this.setLastName = (lastname) => {
+      firstAndLast[1] = lastname;
+    }
+    this.setFullName = (fullname) => {
+      firstAndLast = fullname.split(' ');
+    }
+  };
+  
+  var bob = new Person('Bob Ross');
