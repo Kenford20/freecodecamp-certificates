9f216121bbf1ba2fa152cd2278ae64d0a3fa2771
Kenford <kzhou0066@gmail.com>
2018-10-26 01:04:48 -0400

    added more solutions

--- Javascript Algorithms and Data Structures Certification/intermediate-algorithm-scripting/sum-all-primes.js
+++ Javascript Algorithms and Data Structures Certification/intermediate-algorithm-scripting/sum-all-primes.js
@@ -4,3 +4,24 @@
 
 // The provided number may not be a prime.
 
+function sumPrimes(num) {
+    let primeSum = 0;
+  
+    // produce prime numbers up to 'num'
+    for(let i = 2; i <= num; i++){
+      for(let divisor = 2; divisor <= i; divisor++){
+        if(i != divisor && (i % divisor) === 0){ // check for third divisor (remainder is 0 after division)
+          // not prime, so increment and check next number
+          i++; 
+          divisor=2; // reset divisor
+        }
+      }
+      // only comes to this point if i is prime
+      if(i <= num){
+      primeSum += i;
+      }
+    }
+    return primeSum;
+  }
+  
+  sumPrimes(10);
