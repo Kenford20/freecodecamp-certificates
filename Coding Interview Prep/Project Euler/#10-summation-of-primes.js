147d94c11de5d4b82ade08ad781c48842d7f7e3a
Kenford <kzhou0066@gmail.com>
2018-11-20 23:52:28 -0500

    completed drum machine project, made a piano

--- Coding Interview Prep/Project Euler/#10-summation-of-primes.js
+++ Coding Interview Prep/Project Euler/#10-summation-of-primes.js
@@ -2,18 +2,3 @@
 
 // Find the sum of all the primes below n.
 
-function numberLetterCounts(limit) {
-    // Good luck!
-    let ones = ['one','two','three','four','five','six','seven','eight','nine'];
-    let teens = ['eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
-    let tens = ['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
-  
-    // if >= 100, hundred and = 10 chars always
-    // if >= 10, 1-9 appears every multiple of ten (36 chars) 
-  
-    
-  
-    return true;
-  }
-  
-  numberLetterCounts(5);
